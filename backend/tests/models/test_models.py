973d7626dbe082d46a7dbf25922250422c27ab98
DCharles01 <dcharles@kippnyccp.org>
2024-02-15 22:47:00 -0500

    update test_cryptoprices to test_crypto_prices

--- backend/tests/models/test_models.py
+++ backend/tests/models/test_models.py
@@ -19,7 +19,7 @@ def test_crypto_has_name(create_crypto):
     assert create_crypto.symbol == 'bitcoin'
 
 
-def test_cryptoprices(create_crypto_price):
+def test_crypto_prices(create_crypto_price):
     assert create_crypto_price.price_usd == 52000
     assert create_crypto_price.symbol.symbol == 'bitcoin'
 
